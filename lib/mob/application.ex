ef7d3655d6fb9c6b81d5df3f785319907e23956c
Enrico Mischorr <enrico@mischorr.de>
2020-02-17 18:07:53 +0100

    tune hackney

--- lib/mob/application.ex
+++ lib/mob/application.ex
@@ -22,6 +22,8 @@ defmodule Mob.Application do
       # {Horde.DynamicSupervisor, [name: HelloWorld.HelloSupervisor, strategy: :one_for_one]},
       {Mob.Cluster.NodeListener, []},
       supervisor(Task.Supervisor, [[name: Mob.TaskSupervisor]]),
+      #timeout is the time we keep the connection alive in the pool, max_connections is the number of connections maintained in the pool. Each connection in a pool is monitored and closed connections are removed automatically.
+      :hackney_pool.child_spec(:mob_pool, [timeout: 15000, max_connections: 1000])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- lib/mob/http.ex
+++ lib/mob/http.ex
@@ -1,10 +1,12 @@
 defmodule Mob.Http do
   use HTTPoison.Base
 
+  @default_options [ssl: [{:verify, :verify_none}], follow_redirect: true, max_redirect: 5, "User-Agent": "flash-mob", hackney: [pool: :mob_pool, headers: [{"User-Agent", "flash-mob"}]]]
+
   def request(atom, url, body, headers, options) do
     # TODO: use hackney metrics
     started_at = System.monotonic_time(:microsecond)
-    result = super(atom, url, body, headers, options ++ [{"User-Agent", "flash-mob"}])
+    result = super(atom, url, body, headers, @default_options ++ options)
     finished_at = System.monotonic_time(:microsecond)
     resp_time = finished_at - started_at
     {status, response} = result
